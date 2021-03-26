8916390c8ae92f441d0921fadcedb81514e0cab4
Calvin Fredicson <calvin.fredicson@gmail.com>
2021-03-27 00:06:05 +0800

    fb-ui css

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDXLBDNqA7RhHsuduXPpcKQAVJmVPgJuQs",
+    authDomain: "facebook-66aca.firebaseapp.com",
+    projectId: "facebook-66aca",
+    storageBucket: "facebook-66aca.appspot.com",
+    messagingSenderId: "1004746437970",
+    appId: "1:1004746437970:web:364ad21760004e2a9cb68b",
+    measurementId: "G-GVNZ6VDS8M"
+  };
